041d79e1f52e6f96a7b9df41da1646bc9f15548a
SuperSaiyanAsian <25292225+SuperSaiyanAsian@users.noreply.github.com>
2021-04-13 19:04:33 -0700

    Added code for Exercise 3

--- Homework1/HW1.hs
+++ Homework1/HW1.hs
@@ -11,3 +11,22 @@ import HW1types
 
 
 -- Exercise 3: Data Types
+width :: Shape -> Length
+width (Pt _) = 0
+width (Circle _ rad) = rad * 2
+width (Rect _ x _) = x
+
+bbox :: Shape -> BBox
+bbox (Pt point) = (point,point)
+bbox (Circle (x,y) rad) = ((x-rad,y-rad),(x+rad,y+rad))
+bbox (Rect (x,y) wid len) = ((x,y),(x+wid,y+len))
+
+minX :: Shape -> Number
+minX (Pt (x,y)) = x
+minX (Circle (x,y) rad) = x-rad
+minX (Rect (x,y) wid len) = x
+
+move :: Shape -> Point -> Shape
+move (Pt (x,y)) (x2,y2) = Pt (x+x2,y+y2)
+move (Circle (x,y) rad) (x2,y2) = Circle (x+x2,y+y2) rad
+move (Rect (x,y) wid len) (x2,y2) = Rect (x+x2,y+y2) wid len
