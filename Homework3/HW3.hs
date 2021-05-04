2f8c8565f0cbdd037aeec74757b0400d1897e372
Srikar Valluri <valluris@oregonstate.edu>
2021-05-04 16:58:06 -0700

    added stuff

--- Homework3/HW3.hs
+++ Homework3/HW3.hs
@@ -1,15 +1,27 @@
 -- Exercise 1: A Stack Language (Srikar)
 
-type Prog = [Cmd]
+
+
+
 
 data Cmd = LD Int
-| ADD
-| MULT
-| DUP
+         | ADD
+         | MULT
+         | DUP
+         deriving Show
+
+type Prog = [Cmd]
 
 type Stack = [Int]
 type D = Stack -> Stack
 
 semCmd :: Cmd -> D
+semCmd (LD i) stack = [i] ++ stack 
+semCmd ADD stack = (stack!!0 + stack!!1):tail(tail stack)
+semCmd MULT stack = (stack!!0 * stack!!1):tail(tail stack)
+semCmd DUP stack = (stack!!0):stack
 
 sem :: Prog -> D
+sem (x:xs) stack = semCmd x stackUpdated
+                   where stackUpdated = sem xs stack
+
