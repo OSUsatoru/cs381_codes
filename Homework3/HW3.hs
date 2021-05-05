d2715c67a5fe236fad24491f1159aa17e22704de
OSUsatoru <yamamsat@oregonstate.edu>
2021-05-04 19:23:45 -0700

    edit some of lines

--- Homework3/HW3.hs
+++ Homework3/HW3.hs
@@ -1,9 +1,5 @@
 -- Exercise 1: A Stack Language (Srikar, Satoru)
 
-
-
-
-
 data Cmd = LD Int
          | ADD
          | MULT
@@ -16,10 +12,10 @@ type Stack = [Int]
 type D = Stack -> Stack
 
 semCmd :: Cmd -> D
-semCmd (LD i) stack = [i] ++ stack 
+semCmd (LD i) stack = [i] ++ stack
 semCmd ADD stack = (head(evalS stack) + (evalS stack)!!1):tail(tail (evalS stack))
 semCmd MULT stack = (head(evalS stack) * (evalS stack)!!1):tail(tail (evalS stack))
-semCmd DUP stack = (head(evalDUP stack)):(evalDUP stack)
+semCmd DUP stack = (head(evalD stack)):(evalD stack)
 
 sem :: Prog -> D
 sem [] stack = stack
@@ -30,10 +26,17 @@ stackLength [] = 0
 stackLength (x:xs) = 1 + stackLength xs
 
 evalS :: Stack -> Stack
-evalS x = if (stackLength x) > 1 then x else error("There are less than 2 elements in the stack.")
+evalS x = case (stackLength x) of
+                len -> if len > 1 then x
+                       else error("There are less than 2 elements in the stack.")
 
-evalDUP :: Stack -> Stack
-evalDUP x = if (stackLength x) > 0 then x else error("There are no elements in the stack to be duplicated.")
+--evalS x = if (stackLength x) > 1 then x else error("There are less than 2 elements in the stack.")
+
+evalD :: Stack -> Stack
+evalD x = case (stackLength x) of
+                len -> if len > 0 then x
+                       else error("Expression DUP There are less than 1 elements in the stack.")
+--evalD x = if (stackLength x) > 0 then x else error("There are no elements in the stack to be duplicated.")
 
 run :: Prog -> Stack
 run p = sem p []
