5d1d7608e34cd3525624c91251b3b3ed04dffa59
OSUsatoru <yamamsat@oregonstate.edu>
2021-05-04 19:23:12 -0700

    edit some of lines

--- semantics/Expr2.hs
+++ semantics/Expr2.hs
@@ -1,4 +1,4 @@
--- 
+--
 -- Two-Type Expression Language
 --
 module Expr2 where
@@ -20,7 +20,7 @@ typeError = Not (N 2)
 
 -- A data type to represent the union type of
 -- semantic values
--- 
+--
 data Val = I Int
          | B Bool
          deriving Show
@@ -28,19 +28,11 @@ data Val = I Int
 -- Semantics maps into union type
 -- No error handling yet.
 --
-sem :: Expr -> Val
-sem (N i)        = I i
-sem (Plus e e')  = case (sem e,sem e') of
-                     (I i,I j) -> I (i+j)
-sem (Equal e e') = case (sem e,sem e') of
-                     (I i,I j) -> B (i==j)
-                     (B b,B c) -> B (b==c)
-sem (Not e)      = case sem e of
-                     B b  -> B (not b)
+
 
 
 -- A semantics definition is like a language interpreter.
--- The following interpreter uses local where definitions 
+-- The following interpreter uses local where definitions
 -- to perform a minimal form of dynamic type checking.
 --
 eval :: Expr -> Val
@@ -61,25 +53,25 @@ bool (B b) = b
 bool v     = error ("Value "++show v++" is not a boolean")
 
 
--- The error messages for type errors are based on the 
+-- The error messages for type errors are based on the
 -- values only and do not indicate the source of error.
 --
--- We can implement an interpreter with slightly better 
--- error reporting by putting the type expectation into 
--- the evaluator directly so that the original expressions 
+-- We can implement an interpreter with slightly better
+-- error reporting by putting the type expectation into
+-- the evaluator directly so that the original expressions
 -- can be exploited as context in error messages.
 --
 evalI :: Expr -> Int
 evalI (N i)       = i
 evalI (Plus e e') = evalI e+evalI e'
 evalI e           = error ("Expression "++show e++
-                           " does not evaluate to an integer") 
+                           " does not evaluate to an integer")
 
 evalB :: Expr -> Bool
 evalB (Equal e e') = evalI e==evalI e'
 evalB (Not e)      = not (evalB e)
 evalB e            = error ("Expression "++show e++
-                           " does not evaluate to a boolean") 
+                           " does not evaluate to a boolean")
 
 evalDynTC :: Expr -> Val
 evalDynTC (N i)        = I i
@@ -91,3 +83,12 @@ e1 = N 2 `Equal` N 2
 e2 = N 1 `Equal` e1
 e3 = Not (N 2 `Plus` N 2)
 
+sem :: Expr -> Val
+sem (N i)        = I i
+sem (Plus e e')  = case (sem e,sem e') of
+                     (I i,I j) -> I (i+j)
+sem (Equal e e') = case (sem e,sem e') of
+                     (I i,I j) -> B (i==j)
+                     (B b,B c) -> B (b==c)
+sem (Not e)      = case sem e of
+                     B b  -> B (not b)
