a0f87b6933152e8139d1c3762a9be620ee1db24b
SuperSaiyanAsian <25292225+SuperSaiyanAsian@users.noreply.github.com>
2021-05-16 18:03:12 -0700

    Upload complete Exercise 1

--- Homework4/HW4.hs
+++ Homework4/HW4.hs
@@ -1,3 +1,5 @@
+-- Exercise 1. A Rank-Based Type System for the Stack Language (Satoru, Lance)
+
 type Prog = [Cmd]
 type Stack = [Int]
 
@@ -23,7 +25,6 @@ rankC (DEC) = (1,1)
 rankC (SWAP) = (2,2)
 rankC (POP x) = (x,0)
 
---Rank = fst element - scd element
 rank :: Prog -> Rank -> Maybe Rank
 rank [] r = Just r
 rank (x:xs) r | r >= n = rank xs (r-n+m)
@@ -35,22 +36,38 @@ rankP [] = Just 0
 rankP x = rank x 0
 
 --(b)
-semStatTC :: Prog -> Maybe Stack
-semStatTC x | not(r == Nothing) = Just(sem x [])
-            | otherwise = Nothing
-              where r = rankP x
+-- semStatTC :: Prog -> Maybe Stack
+-- semStatTC x | rankP x /= Nothing = Just(sem x [])
+--             | otherwise = Nothing 
 
-semCmd :: Cmd -> Stack -> Stack
-semCmd (LD i) (s)          = (i:s)
-semCmd DUP    (vs@(v:_))   = (v:vs)
-semCmd ADD    (v1:v2:vs)   = (v1+v2:vs)
-semCmd MULT   (v1:v2:vs)   = (v1*v2:vs)
-semCmd DEC    (v:vs)       = (v-1,vs)
-semCmd SWAP   (v1:v2:vs)   = (v2:v1:vs)
-semCmd (POP x) s           =
+-- rankP does error checking already so sem's
+-- type and definition can be simplified like so:
 
-sem :: Prog -> Stack -> Stack
+-- sem :: Prog -> Stack -> Stack
+-- sem []     s = s
+-- sem (c:cs) s = sem cs (semCmd c s)
+
+-- semCmd :: Cmd -> Stack -> Stack
+-- semCmd (LD x)   s           = (x:s)
+-- semCmd DUP     (vs@(v:_))   = (v:vs)
+-- semCmd ADD     (v1:v2:vs)   = (v1+v2:vs)
+-- semCmd MULT    (v1:v2:vs)   = (v1*v2:vs)
+-- semCmd DEC     (v:vs)       = (v-1:vs)
+-- semCmd SWAP    (v1:v2:vs)   = (v2:v1:vs)
+-- semCmd (POP x)  s           = drop x s
+
+sem :: Prog -> Maybe Stack -> Maybe Stack
 sem []     s = s
 sem (c:cs) s = sem cs (semCmd c s)
 
---2
+semCmd :: Cmd -> Maybe Stack -> Maybe Stack
+semCmd (LD x)  (Just s)          = Just (x:s)
+semCmd DUP     (Just vs@(v:_))   = Just (v:vs)
+semCmd ADD     (Just (v1:v2:vs)) = Just (v1+v2:vs)
+semCmd MULT    (Just (v1:v2:vs)) = Just (v1*v2:vs)
+semCmd DEC     (Just (v:vs))     = Just (v-1:vs)
+semCmd SWAP    (Just (v1:v2:vs)) = Just (v2:v1:vs)
+semCmd (POP x) (Just s)          = Just (drop x s)
+semCmd _       _                 = Nothing
+
+-- Exercise 2. Shape Language
